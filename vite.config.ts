f2b4a60efe2bbd78ebc65590c63d0f7fb0c07713
reedycat <wpsells@gmail.com>
2024-03-22 13:17:43 +0200

    vite config load context fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import {
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import remixConfig from './remix.config'
+import { getLoadContext } from './load-context'
 
 export default defineConfig({
-	plugins: [remixCloudflareDevProxy(), remix(remixConfig), tsconfigPaths()]
+	plugins: [
+		remixCloudflareDevProxy({ getLoadContext }),
+		remix(remixConfig),
+		tsconfigPaths()
+	]
 })
